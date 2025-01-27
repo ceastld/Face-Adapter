099fca64466c85715a56f34b86f1c4aad7954906
ceastld <1950946176@qq.com>
2025-01-27 12:11:30 +0000

    env

--- create_env.sh
+++ create_env.sh
@@ -1,8 +1,7 @@
 conda create -n faceapt python=3.10 -y
 conda activate faceapt
 ml cuDNN/8.7.0.84-CUDA-11.8.0
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install accelerate diffusers==0.26.0 insightface onnxruntime-gpu==1.18.0
+pip install -r requirements.txt
 
 huggingface-cli download FaceAdapter/FaceAdapter --local-dir ./checkpoints
 # huggingface-cli download FaceAdapter/FaceAdapter --filename controlnet/config.json --local-dir ./checkpoints
